05863f49ac2660847b1ab5cee1de24171dd45651
Sosuke Suzuki <aosukeke@gmail.com>
2018-12-04 22:50:46 +0900

    Uncontrolled input

--- src/components/organisms/TodoComponent.tsx
+++ src/components/organisms/TodoComponent.tsx
@@ -1,8 +1,17 @@
-import React, { useCallback, useState } from "react";
+import React, {
+  useCallback,
+  useState,
+  useEffect,
+  useRef,
+  SyntheticEvent,
+  KeyboardEvent
+} from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
 import { Todo } from "../../lib/type";
 import colors from "../../lib/colors";
 import TodoDetail from "./TodoDetail";
+import { ActionTypes } from "../../lib/actionCreators";
 
 const Container = styled.div`
   margin: 5px 0;
@@ -18,39 +27,114 @@ const TodoModal = styled.div`
   background-color: rgba(0, 0, 0, 0.3);
   z-index: 0;
 `;
+const TodoNameInput = styled.input``;
 
-type TodoComponentProps = Todo & {};
+type TodoComponentProps = Todo & {
+  deleteTodo: (
+    { todoId }: { todoId: string }
+  ) => {
+    type: ActionTypes;
+  };
+  updateTodo: (
+    { todo }: { todo: Todo }
+  ) => {
+    type: ActionTypes;
+  };
+};
 
 interface State {
   isShownTodoModal: boolean;
+  isTypingTodoName: boolean;
 }
 
-const initialState: State = {
-  isShownTodoModal: false
-};
-
-const TodoComponent: React.SFC<TodoComponentProps> = ({ ...todo }) => {
+const TodoComponent: React.SFC<TodoComponentProps> = ({
+  deleteTodo,
+  updateTodo,
+  ...todo
+}) => {
   const { name } = todo;
-  const [{ isShownTodoModal }, setState] = useState(initialState);
+
+  const initialState: State = {
+    isShownTodoModal: false,
+    isTypingTodoName: name === ""
+  };
+
+  const todoNameInputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (todoNameInputEl && todoNameInputEl.current) {
+      todoNameInputEl.current.focus();
+    }
+  });
+
+  useEffect(function() {
+    if (name === "")
+      setState((state: State) => ({
+        ...state,
+        isTypingTodoName: true
+      }));
+  }, []);
+
+  const [{ isShownTodoModal, isTypingTodoName }, setState] = useState(
+    initialState
+  );
 
   const onClickTodoComponent = useCallback(function() {
-    setState((state: State) => ({
-      ...state,
-      isShownTodoModal: true
-    }));
+    isTypingTodoName ||
+      setState((state: State) => ({
+        ...state,
+        isShownTodoModal: true
+      }));
   }, []);
 
-  const onClickTodoModal = useCallback(function() {
-    setState((state: State) => ({
-      ...state,
-      isShownTodoModal: false
-    }));
-  }, []);
+  const onClickTodoModal = useCallback(
+    function() {
+      setState((state: State) => ({
+        ...state,
+        isShownTodoModal: false
+      }));
+    },
+    [isTypingTodoName]
+  );
+
+  const onBlurTodoNameInput = useCallback(
+    function() {
+      deleteTodo({ todoId: todo.id });
+    },
+    [todo.id]
+  );
+
+  const onKeyPressTodoNameInput = useCallback(function(
+    ev: KeyboardEvent<HTMLInputElement>
+  ) {
+    if (ev.key === "Enter") {
+      updateTodo({
+        todo: {
+          ...todo,
+          name: todoNameInputEl.current!.value
+        }
+      });
+      setState((state: State) => ({
+        ...state,
+        isTypingTodoName: false
+      }));
+    }
+  },
+  []);
 
   return (
     <>
       <Container onClick={onClickTodoComponent}>
-        <h4>{name || "名前はありません"}</h4>
+        {!isTypingTodoName ? (
+          <h4>{name || "名前はありません"}</h4>
+        ) : (
+          <TodoNameInput
+            type="text"
+            ref={todoNameInputEl}
+            onBlur={onBlurTodoNameInput}
+            onKeyPress={onKeyPressTodoNameInput}
+          />
+        )}
       </Container>
       {isShownTodoModal && (
         <>
@@ -62,4 +146,12 @@ const TodoComponent: React.SFC<TodoComponentProps> = ({ ...todo }) => {
   );
 };
 
-export default TodoComponent;
+export default connect(
+  null,
+  dispatch => ({
+    deleteTodo: ({ todoId }: { todoId: string }) =>
+      dispatch({ type: ActionTypes.DeleteTodo, payload: { todoId } }),
+    updateTodo: ({ todo }: { todo: Todo }) =>
+      dispatch({ type: ActionTypes.UpdateTodo, payload: { todo } })
+  })
+)(TodoComponent);
